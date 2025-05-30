5ed6915a21583e60bf42bf155a787c99cf82baa8
rosescent00 <dahae002@gmail.com>
2025-05-31 01:03:24 +0900

    Create Application.java

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,11 @@
+---
+# 📄 src/main/java/com/example/Application.java
+
+```java
+package com.example;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
+}
